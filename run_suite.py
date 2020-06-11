7c5cdf9537c75d637a686b2d55f3017f7fcd158e
QLING-WO <66556787+QLING-WO@users.noreply.github.com>
2020-06-11 10:40:28 +0800

    Update run_suite.py

--- run_suite.py
+++ run_suite.py
@@ -9,3 +9,5 @@ import app
 suite = unittest.TestLoader().discover(app.BASE_DIR + '/script', pattern='test*.py')
 file_name = "test_BeautifulReport_{}.html".format(time.strftime("%Y%m%d%H%M%S"))
 BeautifulReport(suite).report(filename=file_name,description="测试登录及添加员工模块用例报告", log_path=app.BASE_DIR + '/report')
+print("*" * 30)
+print("测试构建后的代码")
